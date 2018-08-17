5faf459c99cacee342f92e4908230c29f38eb3ce
Admin <nzhang@AUC02WN07SHV2T.local>
2018-08-17 19:31:55 -0400

    update test.js and clean up twitter-q.js
    Making ctype and cparam, the cache mechanism and specifications passed through to cached-throttled-req, user-specified parameters that default to 'file' and './cache/'

--- test/test.js
+++ test/test.js
@@ -6,14 +6,21 @@ var should = chai.should();
 
 var TwitterQ = require('../lib/twitter-q');
 var twq = new TwitterQ({
-  consumer_key:			    'nQLmBshDfiOZ1UJu8LEC09uy2',
-  consumer_secret:		  '2Q0sNEzhXSR7eTRDPQ5Q3ym38Oj89ZzQxH39dmHIRT51wfULOP',
-  access_token:		      '389017354-kfQYYJojSN52rHyflKUWTgVME3V1H0xCVmnyCqHG',
-  access_token_secret:  'yBY2a1ywbYpGmuZA1mvgbQQDSlfJsTNw0ckKaKN8mSwsg',
+  consumer_key:			    process.env.TWITTER_API_KEY,
+  consumer_secret:		  process.env.TWITTER_API_SECRET,
+  access_token:		      process.env.TWITTER_API_TOKEN,
+  access_token_secret:	process.env.TWITTER_API_TOKEN_SECRET,
 });
 
 describe('#TwitterQ with user timeline retrieval', function() {
-  it('respond with matching records', async function() {
-    return twq.get_user_timeline('543000344').should.eventually.have.length.be.above(0);
+  it('respond with matching records', (done) => {
+    twq.get_user_timeline('2485791210')
+      .then(results => {
+        console.log(results[0]);
+        results.should.have.length.be.above(0);
+        done();
+      })
+      .catch(done);
+    // return twq.get_user_timeline('2733452459').should.eventually.have.length.be.above(0);
   });
 });
